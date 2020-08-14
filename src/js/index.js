75b1b893a1c533b6eb1a608c39deb8ebf5c19e2b
z1kman <z1kmanfast@gmail.com>
2020-08-14 22:39:41 +0300

    Испрвил скрытие кнопки меню

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 
 function OnClickMenu(btn){
     let Menu = document.getElementById("Menu-Mobile");
-    btn.classList.contains('Menu-Exit') ? Menu.style.visibility = 'hidden' : Menu.style.visibility = 'visible ' ;
+    let ico = document.getElementById("Menu-ico");
+    if (btn.classList.contains('Menu-Exit')) {
+        Menu.style.visibility = 'hidden';
+        ico.style.visibility = 'visible';
+    } else {
+        Menu.style.visibility = 'visible ';
+        ico.style.visibility = 'hidden';
+    }
 }
