2e2fbc818b969961c352e06e52ecf7aa03720786
Шашков Антон Константинович <z1kmanfast@gmail.com>
2021-02-23 16:10:38 +0300

    Исправил неправильное отображение мобильного меню при ресайзе или изменении ориентации, теперь меню автоматически убирается если произошел ресайз на декстопный экран

---
+++ src/js/data.js
@@ -0,0 +1,62 @@
+
+let content = [{
+    first_name: "alexandra",
+    last_name: "litvinova",
+    src: "./src/pic/Rectangle 17.jpg"
+  },
+  {
+    first_name: "alina",
+    last_name: "sokolova",
+    src: "./src/pic/Rectangle 16.jpg"
+  },
+  {
+    first_name: "anastasia",
+    last_name: "mospalenko",
+    src: "./src/pic/Rectangle 18.jpg"
+  },
+  {
+    first_name: "ANASTASIA",
+    last_name: "YAROTSKOVA",
+    src: "./src/pic/Rectangle 19.jpg"
+  },
+  {
+    first_name: "ANGIE",
+    last_name: "GONOKHOVA",
+    src: "./src/pic/Rectangle 20.jpg"
+  },
+  {
+    first_name: "EKATERINA",
+    last_name: "TERENTIEVA",
+    src: "./src/pic/Rectangle 21.jpg"
+  },
+  {
+    first_name: "kira",
+    last_name: "kopteva",
+    src: "./src/pic/Rectangle 22.jpg"
+  },
+  {
+    first_name: "gala",
+    last_name: "grudinina",
+    src: "./src/pic/Rectangle 23.jpg"
+  },
+  {
+    first_name: "valeria",
+    last_name: "mitt",
+    src: "./src/pic/Rectangle 24.jpg"
+  },
+  {
+    first_name: "diana",
+    last_name: "saha",
+    src: "./src/pic/Rectangle 25.jpg"
+  },
+  {
+    first_name: "lisa",
+    last_name: "moreva",
+    src: "./src/pic/Rectangle 26.jpg"
+  },
+  {
+    first_name: "nastya",
+    last_name: "medvedeva",
+    src: "./src/pic/Rectangle 27.jpg"
+  }
+]
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
+window.onload = function () {
+    GetContent();
+}
+window.onresize = function (event) {
+    checkResizeWindow(window.innerWidth);
+};
 
-function OnClickMenu(btn){
+function OnClickMenu(btn) {
     let Menu = document.getElementById("Menu-Mobile");
     let ico = document.getElementById("Menu-ico");
     if (btn.classList.contains('Menu-Exit')) {
@@ -9,4 +15,52 @@ function OnClickMenu(btn){
         Menu.style.visibility = 'visible ';
         ico.style.visibility = 'hidden';
     }
+}
+
+function checkResizeWindow(width) {
+    let Menu = document.getElementById("Menu-Mobile");
+    let orientation =  window.screen.orientation;
+    let ico = document.getElementById("Menu-ico");
+
+    if (width > 1080 && orientation.type == 'landscape-primary') {
+        Menu.style.visibility = 'hidden';
+        ico.style.visibility = 'visible';
+    } else if (width <= 1080 || orientation === 'portrait-primary') {
+    
+ 
+    }
+
+}
+
+function GetContent() {
+    let list = document.getElementById('container');
+    content.map(function (elem) {
+        let li = document.createElement('li');
+        let img = document.createElement('img');
+        let divName = document.createElement('div');
+        let firstName = document.createElement('div');
+        let lastName = document.createElement('div');
+
+        li.className = 'Container__Item';
+        img.src = elem.src;
+        divName.className = 'Name';
+        firstName.className = 'Name__First-Name';
+        lastName.className = 'Name__Last-Name';
+
+        lastName.innerText = elem.last_name;
+        firstName.innerText = elem.first_name;
+
+        divName.appendChild(firstName);
+        divName.appendChild(lastName);
+
+        li.appendChild(img);
+        li.appendChild(divName);
+
+        list.appendChild(li);
+
+
+    })
+
+
+
 }
