46e15d0bc8f8485246abbd98cb6dfbab861d63b7
Anton Shashkov <z1kmanfast@gmail.com>
2022-11-23 16:07:41 +0300

    Final Header

--- valvet-demo/src/components/Header/index.tsx
+++ valvet-demo/src/components/Header/index.tsx
@@ -2,11 +2,13 @@ import styles from "./styles.module.scss";
 
 // Components
 import { Logo } from "../Logo";
+import { Link } from "../Link";
 
 export function Header() {
   return (
     <header className={styles.Header}>
-      <div className={styles.Header__Logo}>
+      <div className={styles.Header__Items}>
+        <Link label="back" color="secondary" italic size="s"/>
         <Logo />
       </div>
     </header>
