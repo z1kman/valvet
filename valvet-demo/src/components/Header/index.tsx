d04ac5625785f4b69f5fb9d98f8855013ac73bb2
Anton Shashkov <z1kmanfast@gmail.com>
2022-11-24 17:46:49 +0300

    Add mobile version fo header

--- valvet-demo/src/components/Header/index.tsx
+++ valvet-demo/src/components/Header/index.tsx
@@ -3,12 +3,18 @@ import styles from "./styles.module.scss";
 // Components
 import { Logo } from "../Logo";
 import { Link } from "../Link";
+import { Icon } from "../Icon";
 
 export function Header() {
   return (
     <header className={styles.Header}>
       <div className={styles.Header__Items}>
-        <Link label="back" color="secondary" italic size="s"/>
+        <div className={styles.Header__Item_link}>
+          <Link label="back" color="secondary" italic size="s" />
+        </div>
+        <div className={styles.Header__Item_icon}>
+          <Icon name="menu"/>
+        </div>
         <Logo />
       </div>
     </header>
